60837f0d719e3666eefc0741bb46e5ca860910f5
Efe <efenadir@sabanciuniv.edu>
2020-12-01 18:47:39 +0300

    home-page-add

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import Home from "./Home.js";
 
-function App(){
-    return (
-        <>
-        <h1>AAAAAA</h1>
-        <div>
-            Artic
-        </div>
-        </>
-    );
+function App() {
+  return (
+    <>
+      <h1>AAAAAA</h1>
+      <div>Artic</div>
+      <Home />
+    </>
+  );
 }
-
 
 export default App;
