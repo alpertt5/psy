68c3e542e27236d336423adafd7ababecdf627b0
Alper <tellioglualper@gmail.com>
2020-12-05 15:47:18 +0300

    add animation to presentation page

--- src/pages/Presentations/Presentations.js
+++ src/pages/Presentations/Presentations.js
@@ -2,6 +2,8 @@ import React, {useState} from 'react'
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 import "./Presentations.css";
 import presentationList from "./PresentationList";
+import Fade from 'react-reveal/Fade';
+import Zoom from 'react-reveal/Zoom';
 
 
 const Presentations = () => {
@@ -9,9 +11,12 @@ const Presentations = () => {
 
 
     return (
-        <ReactCSSTransitionGroup
-        transitionName="example" transitionAppear={true}
-        transitionAppearTimeout={700}>
+        // <ReactCSSTransitionGroup
+        // transitionName="example" transitionAppear={true}
+        // transitionAppearTimeout={700}>
+        <Zoom>
+
+        
 
         <div className="presentations-container">
             {presentationList.map((listItem, index) => (
@@ -20,26 +25,44 @@ const Presentations = () => {
                     !(index % 2)
                     ? 
                     <>
+
                     <div key={index} className="presentation-card">
+                        <Fade left>
+
                         <div className="flex-item">
                             <h1> {listItem.title} </h1>
                             <p> {listItem.content}</p>
                         </div>
+                        </Fade>
+
+
+                        <Fade right>
+
                         <div className="flex-item">
                             <img alt="image" src={require( `${listItem.imageUrl}`).default}/>
                         </div>
+
+                        </Fade>
                     </div>
                     </> 
                     : 
                     <>
                     <div key={index} className="presentation-card flex-reverse">
+
+                        <Fade left>
+
                         <div className="flex-item">
                             <img alt="image" src={require( `${listItem.imageUrl}`).default}/>
                         </div>
+                        </Fade>
+
+                        <Fade right>
+                            
                         <div className="flex-item">
                             <h1> {listItem.title} </h1>
                             <p> {listItem.content}</p>
                         </div>
+                        </Fade>
                     </div>
                     </> 
                     }
@@ -48,8 +71,9 @@ const Presentations = () => {
             ))}
             
         </div>
+        </Zoom>
 
-        </ReactCSSTransitionGroup>
+        // </ReactCSSTransitionGroup>
     );
 }
 
